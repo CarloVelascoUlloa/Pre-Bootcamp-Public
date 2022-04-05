61506dc7ebac70000ee24570ad12b0d61c152232
CarloVelascoUlloa <98000529+CarloVelascoUlloa@users.noreply.github.com>
2022-04-05 18:54:08 -0400

    Add files via upload

---
+++ JavaScript Coursework/javascript.js
@@ -0,0 +1,25 @@
+function logIn(element) {
+  if (element.innerText =="Login") {
+        element.innerText = "Logout";
+  } else {
+    element.innerText = "Login";
+  }
+}
+
+function hide(element) {
+  element.remove();
+}
+
+function Message (element) {
+  alert ("Ninja Was Liked") 
+    if (element.innerText == "13 likes") {
+          element.innerText = "14 likes";
+    }
+  }
+
+  function message(element) {
+    alert ("Ninja Was Liked") 
+      if (element.innerText == "37 likes") {
+            element.innerText = "38 likes";
+      }
+    }
